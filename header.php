17b834e7dd71a3851441aa2e8be7c79eab720b0c
Rafał Kulawiak <ouix42@gmail.com>
2015-06-17 14:45:28 +0200

    Start

---
+++ header.php
@@ -0,0 +1,50 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Bootstrap 101 Template</title>
+		<link href="css/bootstrap.css" rel="stylesheet">
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<nav class="navbar navbar-default navbar-fixed-top">
+			<div class="container">
+				<div class="navbar-header">
+					<!-- Wersja mobilna: -->
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">Nagłówek</a>
+				</div>
+				<div class="collapse navbar-collapse" id="navbar-collapse">
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="">Menu1</a></li>
+						<li><a href="">Menu2</a></li>
+						<li><a href="">Menu3</a></li>
+						<li><a href="">Menu4</a></li>
+						<li><a href="">Menu5</a></li>
+						<li class="dropdown">
+							<a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Menu6 <span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="">Podmenu1</a></li>
+								<li><a href="">Podmenu2</a></li>
+								<li><a href="">Podmenu3</a></li>
+								<li><a href="">Podmenu4</a></li>
+								<li role="separator" class="divider"></li>
+								<li><a href="">Podmenu5</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+
+		<main>
+			<div class="container">
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include("header.php"); ?>
+
+Treść strony...
+
+<?php include("footer.php"); ?>
